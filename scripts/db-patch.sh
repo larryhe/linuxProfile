fc012a03c6c6bb4e27348b31c5c75a4fbd23f1fc
Larry He <larry.he@veevasystems.com>
2013-07-12 17:49:56 -0700

    remove useless files

--- scripts/db-patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $SF_HOME
-ant upgrade -Dargs="-w jboss -c larry -dontask"
-ant upgrade -Dargs="-w jboss -c larry -createseq"
-ant upgrade -Dargs="-w jboss -c larry -createindices"
--- scripts/jboss_run_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd $JBOSS_HOME
-./bin/run.sh -c main -b 0.0.0.0
