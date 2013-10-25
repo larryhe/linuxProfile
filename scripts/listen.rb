e0835c224e3da3ab2ebc2ffd2db8749767bacce1
Larry He <larry.he@veeva.com>
2013-10-25 11:38:23 -0700

    add listen module to utitlity tool to monitor file systems changes using listen gem of ruby

---
+++ scripts/listen.rb
@@ -0,0 +1,15 @@
+#!/usr/local/bin/ruby
+require 'listen'
+PROJ_DIR="/Users/larryhe/workspace/vault.git"
+SRC_JMVC=PROJ_DIR + "/WzlUI/src/main/webapp/JMVC" 
+SRC_UI_VIEW=PROJ_DIR + "/WzlUI/src/main/webapp/WEB-INF/views"
+SRC_VMC_VIEW=PROJ_DIR + "/WzlVMC/src/main/webapp/WEB-INF/views"
+SRC_ANNOTATE=PROJ_DIR + "/AnnotateApp/src/main/webapp/annotate"
+
+listener = Listen.to(SRC_JMVC, SRC_UI_VIEW, SRC_VMC_VIEW, SRC_ANNOTATE) do |modified|
+    modified.each do |f|
+        system("exe_watchdog #{f}")
+    end
+end
+listener.start # not blocking
+sleep
--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -3,7 +3,7 @@ PROJ_DIR="/Users/larryhe/workspace/vault.git"
 SRC_JMVC=$PROJ_DIR/WzlUI/src/main/webapp/JMVC 
 SRC_UI_VIEW=$PROJ_DIR/WzlUI/src/main/webapp/WEB-INF/views 
 SRC_VMC_VIEW=$PROJ_DIR/WzlVMC/src/main/webapp/WEB-INF/views 
-SRC_ANNOTATE=$PROJ_DIR/Annotate/src/main/webapp/annotate
+SRC_ANNOTATE=$PROJ_DIR/AnnotateApp/src/main/webapp/annotate
 watchmedo shell-command --pattern="*.js;*.jspx;*.css" --recursive --command='\
-watchdog_execute "${watch_src_path}"' \
-	$SRC_JMVC $SRC_UI_VIEW $SRC_VMC_VIEW $SRC_ANNOTATE_JS \
+exe_watchdog "${watch_src_path}"' \
+	$SRC_JMVC $SRC_UI_VIEW $SRC_VMC_VIEW $SRC_ANNOTATE\
