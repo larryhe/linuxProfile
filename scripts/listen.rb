085b7640a9267305f982b6753027ae5fcc44b663
Larry He <larry.he@veeva.com>
2014-01-28 18:21:59 -0800

    remove vim configuration and update gitignore config

--- scripts/listen.rb
+++ scripts/listen.rb
@@ -5,8 +5,9 @@ SRC_JMVC=PROJ_DIR + "/WzlUI/src/main/webapp/JMVC"
 SRC_UI_VIEW=PROJ_DIR + "/WzlUI/src/main/webapp/WEB-INF/views"
 SRC_VMC_VIEW=PROJ_DIR + "/WzlVMC/src/main/webapp/WEB-INF/views"
 SRC_ANNOTATE=PROJ_DIR + "/AnnotateApp/src/main/webapp/annotate"
+SRC_PHP=PROJ_DIR + "/WzlUI/src/main/webapp/annotate"
 
-listener = Listen.to(SRC_JMVC, SRC_UI_VIEW, SRC_VMC_VIEW, SRC_ANNOTATE) do |modified|
+listener = Listen.to(SRC_JMVC, SRC_UI_VIEW, SRC_VMC_VIEW, SRC_ANNOTATE, SRC_PHP) do |modified|
     modified.each do |f|
         system("exe_watchdog #{f}")
     end
